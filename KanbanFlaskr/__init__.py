670e8abde95ef6baf89bea4e033aa8f968ce1a00
Vicki Petrova <90992029+vickipetrova@users.noreply.github.com>
2022-11-05 13:51:53 -0300

    Documented all of the code. Added unique UUID id's for each task. Improved the design of the kanban board. Fixed the bug where the form was resubmitted every time the page is refreshed. The only thing left is adding the tests.

--- KanbanFlaskr/__init__.py
+++ KanbanFlaskr/__init__.py
@@ -6,8 +6,8 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY='IBelieveICanFly',
+        DATABASE=os.path.join(app.instance_path, 'kanban.sqlite'),
     )
 
     if test_config is None:
@@ -23,11 +23,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    
-    
-
     return app
 
+# Initialize the app. 
 app = create_app()
 
 from KanbanFlaskr import routes
--- KanbanFlaskr/database.py
+++ KanbanFlaskr/database.py
@@ -1,43 +1,92 @@
-import random
-def get_name():
-    return random.choice(["Helen", "Vicki"])
+import uuid
 
+# Store the tasks as a list of dictionaries.
+# No need for a Task class because we won't use any additional functionalities. 
 tasks = [
-        {
-            'id':1, 
-            'name':'Task1', 
-            'description':'Description of Task1',
-            'status': 'To-do'
-        },
-        {
-            'id':2,
-            'name':'Task2', 
-            'description':'Description of Task2',
-            'status':'In progress'
-        },
-        {
-            'id':3,
-            'name':'Task3', 
-            'description':'Description of Task3',
-            'status':'Completed'
-        }
+    # Create static tasks (always pop up in the table). 
+        # {
+        #     'id':1, 
+        #     'name':'Task1', 
+        #     'description':'Description of Task1',
+        #     'status': 'To-do'
+        # },
+        # {
+        #     'id':2,
+        #     'name':'Task2', 
+        #     'description':'Description of Task2',
+        #     'status':'In progress'
+        # },
+        # {
+        #     'id':3,
+        #     'name':'Task3', 
+        #     'description':'Description of Task3',
+        #     'status':'Completed'
+        # }
 ]
 
-def get_tasks(): return tasks 
+def get_tasks(): 
+    """
+    A function to fetch the existing tasks.
+
+    Parameters
+    ----------
+    None
+
+    Output
+    ------
+    tasks: list of dicts
+        Each dictionary represents a task with id, name, description, and status. 
+    """
+    return tasks 
 
 
 def add_task(task_name, task_description):
+    """
+    A function to add a new task to the list of existing tasks. 
+
+    Parameters
+    ----------
+    task_name: str
+        The name of the task.
+    task_description: str
+        The description of the task.
+
+    Output
+    ------
+    None
+    """
 
     tasks.append({
-        'id': len(tasks) + 1,
+        'id': uuid.uuid4(), # create a unique id
         'name': task_name,
         'description': task_description,
         'status': "To-do"
     })
 
 def update_task_status(task_id):
+    """
+    A function to change the status of an existing task. 
+
+    The change of status is incremental: 
+    - "To-do" becomes "In progress"
+    - "In progress" becomes "Completed"
+    - "Completed" becomes "To-do"
+
+    Parameters
+    ----------
+    task_id: UUID4
+        The unique id of the task,
+
+    Output
+    ------
+    None
+    """
+
+    # Find the task using the task id. 
     for task in tasks:
-        if task['id'] == task_id:
+
+        if str(task['id']) == task_id:
+            # Change the status accordingly. 
             if task['status'] == "To-do":
                 task['status']  = "In progress"
             elif task['status'] == "In progress":
@@ -49,7 +98,20 @@ def update_task_status(task_id):
             break
         
 def delete_task(task_id):
+    """
+    A function to delete an existing task. 
+
+    Parameters
+    ----------
+    task_id: UUID
+        The unique ID of the task to be deleted.
+
+    Output
+    ------
+    None
+    """
+    # Find the task using the id and remove it from the list. 
     for i, task in enumerate(tasks):
-        if task['id'] == task_id:
+        if str(task['id']) == task_id:
             tasks.pop(i)
 
